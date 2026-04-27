82277bd99690e385f6787bb056a6a058c7f0832d
Paul Enache <pol@Pols-MacBook-Pro.local>
2026-04-27 21:57:21 +0300

    [CD] Add deployment and integration test stages

--- src/test/java/ro/unibuc/prodeng/IntegrationTestBase.java
+++ src/test/java/ro/unibuc/prodeng/IntegrationTestBase.java
@@ -14,27 +14,41 @@ public abstract class IntegrationTestBase {
 
     private static final String MONGO_URL_ENV = System.getenv("MONGODB_CONECTION_URL");
 
-    private static final MongoDBContainer mongoDBContainer;
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer();
+
+    // static {
+    //     if (MONGO_URL_ENV == null || MONGO_URL_ENV.isEmpty()) {
+    //         mongoDBContainer = new MongoDBContainer("mongo:6.0.20")
+    //                 .withExposedPorts(27017)
+    //                 .withSharding()
+    //                 .withLabel("ro.unibuc.prodeng", "integration-test-mongo");
+    //         mongoDBContainer.start();
+    //     } else {
+    //         mongoDBContainer = null;
+    //     }
+    // }
+
+    // @DynamicPropertySource
+    // static void setProperties(DynamicPropertyRegistry registry) {
+    //     if (MONGO_URL_ENV != null && !MONGO_URL_ENV.isEmpty()) {
+    //         registry.add("mongodb.connection.url", () -> MONGO_URL_ENV);
+    //     } else {
+    //         registry.add("mongodb.connection.url",
+    //                 () -> "mongodb://localhost:" + mongoDBContainer.getMappedPort(27017));
+    //     }
+    // }
 
     static {
-        if (MONGO_URL_ENV == null || MONGO_URL_ENV.isEmpty()) {
-            mongoDBContainer = new MongoDBContainer("mongo:6.0.20")
-                    .withExposedPorts(27017)
-                    .withSharding()
-                    .withLabel("ro.unibuc.prodeng", "integration-test-mongo");
+        if (System.getenv("MONGODB_CONECTION_URL") == null) {
             mongoDBContainer.start();
-        } else {
-            mongoDBContainer = null;
         }
     }
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        if (MONGO_URL_ENV != null && !MONGO_URL_ENV.isEmpty()) {
-            registry.add("mongodb.connection.url", () -> MONGO_URL_ENV);
-        } else {
-            registry.add("mongodb.connection.url",
-                    () -> "mongodb://localhost:" + mongoDBContainer.getMappedPort(27017));
+        if (mongoDBContainer.isRunning()) {
+            String mongoUrl = "mongodb://localhost:" + mongoDBContainer.getMappedPort(27017);
+            registry.add("mongodb.connection.url", () -> mongoUrl);
         }
     }
 }
